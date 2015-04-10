4b10baf1c3a530cd835c3f1f5207c14c53806e1d
Denis <d.kalinichenkooo@gmail.com>
2015-04-11 01:17:10 +0200

    Express Sessions, second step

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var conf = require('../conf');
 
-//var func = require('../func/auth.js');
 
 /* GET home page. */
 
--- routes/login.js
+++ routes/login.js
@@ -16,11 +16,26 @@ var User = require('../models/user.js');
 router.get('/', function(req, res, next) {
     res.render('login', { conf: conf, title: 'Login' });
 }).post('/', function(req, res, next) {
-    sess=req.session;
-    //In this we are assigning email to sess.email variable.
-    //email comes from HTML page.
-    sess.username=req.body.login;
-    res.end('done');
+    var login = req.body.login;
+    var password = req.body.password;
+
+    User.findOne({username: login}, function (err, user) {
+        if (user) {
+            if(user.password === md5(password)) {
+                sess=req.session;
+                //In this we are assigning email to sess.email variable.
+                //email comes from HTML page.
+                sess.username=req.body.login;
+                res.redirect("/");
+            } else {
+                res.send("uncorrect pass!");
+            }
+        } else {
+            res.send("user not exists");
+        }
+    });
+
+
 });
 
 module.exports = router;
