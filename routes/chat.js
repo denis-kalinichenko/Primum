c12c9fde777c6e8892c201b9a7eb5f9af93e845d
Denis Kalinichenko <d.kalinichenkooo@gmail.com>
2015-05-19 13:42:27 +0200

    checkAuth method

--- routes/chat.js
+++ routes/chat.js
@@ -5,58 +5,43 @@ var config = require('config');
 var User = require('models/user').User;
 var AuthError = require('models/user').AuthError;
 
-router.get('/', function(req, res, next) {
-    sess=req.session;
-    if(sess.username) {
-        res.render('chat', { title: 'Primum', logged: true, session: sess });
-    } else {
-        res.redirect("/login");
+var checkAuth = require('middleware/checkAuth');
+
+router.get('/', checkAuth, function(req, res, next) {
+    res.render('chat', { title: 'Primum', logged: true, session: req.session });
+}).get('/search', checkAuth, function(req, res, next) {
+    if(req.query.username == req.session.username) {
+        var result = {
+          "error": 1,
+           "error_msg": "Its your username"
+        };
+        return res.send(JSON.stringify(result));
     }
-}).get('/search', function(req, res, next) {
-    sess=req.session;
-    if(sess.username) {
-        if(req.query.username == sess.username) {
+    User.searchByUsername(req.query.username, "user_id name username -_id ", function(err, user) {
+        if(err) {
+            if (err instanceof AuthError) {
+                return res.send(err.message);
+            } else {
+                next(err);
+            }
+        }
+        if(user != null) {
             var result = {
-              "error": 1,
-               "error_msg": "Its your username"
+                user_id: user.user_id,
+                username: user.username,
+                name: user.name
+            };
+            res.send(JSON.stringify(result));
+        } else {
+            var result = {
+                "error": 1,
+                "error_msg": "Cannot find user"
             };
             return res.send(JSON.stringify(result));
         }
-        User.searchByUsername(req.query.username, "user_id name username -_id ", function(err, user) {
-            if(err) {
-                if (err instanceof AuthError) {
-                    return res.send(err.message);
-                } else {
-                    next(err);
-                }
-            }
-            if(user != null) {
-                var result = {
-                    user_id: user.user_id,
-                    username: user.username,
-                    name: user.name
-                };
-                res.send(JSON.stringify(result));
-            } else {
-                var result = {
-                    "error": 1,
-                    "error_msg": "Cannot find user"
-                };
-                return res.send(JSON.stringify(result));
-            }
-        });
-    } else {
-        res.redirect("/login");
-    }
-}).post('/add', function(req, res, next) {
-    sess=req.session;
-
-    if(sess.username) {
-        res.send(req.body.id);
-    } else {
-        res.redirect("/login");
-    }
-
+    });
+}).post('/add', checkAuth, function(req, res, next) {
+    res.send(req.body.id);
 });
 
 module.exports = router;
